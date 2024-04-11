a5d6371d9e249f762accb582f131c58ee5aa14b6
Sandy Gomez <sgomez1029@gmail.com>
2024-04-11 14:08:32 -0700

    Lesson 2 Assignment (Bar Chart)

--- Lesson2Assignment/ContentView.swift
+++ Lesson2Assignment/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import Charts
+
+struct EDCSchedule: Identifiable {
+    var day: String
+    var stage: String
+    var votes: Int
+    var id = UUID()
+}
+
+var MyEDCSchedule: [EDCSchedule] = [
+    .init(day: "Friday", stage: "Techno", votes: 8),
+    .init(day: "Friday", stage: "HardStyle", votes: 4),
+    .init(day: "Friday", stage: "Drum & Bass", votes: 3),
+    .init(day: "Saturday", stage: "Techno", votes: 4),
+    .init(day: "Saturday", stage: "HardStyle", votes: 5),
+    .init(day: "Saturday", stage: "Drum & Bass", votes: 6),
+    .init(day: "Sunday", stage: "Techno", votes: 4),
+    .init(day: "Sunday", stage: "HardStyle", votes: 8),
+    .init(day: "Sunday", stage: "Drum & Bass", votes: 3)
+]
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart(MyEDCSchedule) { x in
+                BarMark(x: .value("Day", x.day),
+                        y: .value("Votes", x.votes))
+                .foregroundStyle(by: .value("Stage", x.stage))}
         }
-        .padding()
     }
 }
 
